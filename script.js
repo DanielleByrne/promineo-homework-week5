1fc2350eb3fcf8d684aa256778793524a8a2644f
Danielle Byrne <DanielleByrne421@gmail.com>
2021-12-04 18:00:16 -0800

    methods and functionality done, don't know why name isn't working

--- script.js
+++ script.js
@@ -33,11 +33,12 @@ class GameType {
 
 class Options {
   constructor() {
-    this.teams = [];
+    this.games = [];
     this.selectedGame = null;
   }
   start() {
     let selection = this.showOptions();
+
     while (selection != 0) {
       switch (selection) {
         case "1":
@@ -54,5 +55,74 @@ class Options {
       }
       selection = this.showOptions();
     }
+
+    alert("Bye!");
+  }
+  showOptions() {
+    return prompt(`
+     0) exit
+     1) create a category
+     2) view a category
+     3) delete a category
+     `);
+  }
+
+  showGameOptions(gameInfo) {
+    return prompt(
+      `0) back
+           1) create game
+           2) delete game
+           ---------------
+           ${gameInfo}`
+    );
+  }
+
+  createCategory() {
+    let name = prompt("Enter name for new game:");
+    this.games.push(new GameType(name));
+  }
+
+  viewCategory() {
+    let index = prompt("Enter index of Category you want to view");
+    if (index > -1 && index < this.games.length) {
+      this.selectedGame = this.games[index];
+      let description = `Category Name: ${this.selectedGame.name}`;
+      for (let i = 0; i < this.selectedGame.games.length; i++) {
+        description +=
+          i +
+          `) ${this.selectedGame.games[i].name} - ${this.selectedGame.games[i].rating}`;
+      }
+      let selection = this.showGameOptions(description);
+      switch (selection) {
+        case "1":
+          this.createGame();
+          break;
+        case "2":
+          this.deleteGame();
+      }
+    }
+  }
+  deleteCategory(){
+      let index=  prompt("Enter the index of the game you want to delete")
+      if (index > -1 && index < this.games.length){
+          this.games.splice(index, 1)
+      }
+  }
+
+
+  createGame() {
+    let name = prompt("Enter name for new game");
+    let rating = prompt("Enter rating for new game");
+    this.selectedGame.games.push(new Game(name, rating));
+  }
+
+  deleteGame() {
+    let index = prompt("Enter index of the game you want to delete: ");
+    if (index > -1 && index < this.selectedGame.games.length) {
+      this.selectedGame.games.splice(index, 1);
+    }
   }
 }
+
+let options = new Options();
+options.start();
